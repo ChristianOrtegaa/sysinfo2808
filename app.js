0abd6f079d38f6804b2de92a248c401b3a7b389a
ChristianOrtegaa <79687852+ChristianOrtegaa@users.noreply.github.com>
2021-03-10 13:06:37 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var session = require('express-session');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -17,6 +19,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//sesiones
+app.use(session({
+  resave:false,
+  saveUninitialized:true
+}));
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 //boostrap
 app.use(express.static(path.join(__dirname, '/node_modules/bootstrap/dist')));
--- users.js
+++ users.js
@@ -1,14 +1,35 @@
 var express = require('express');
 var router = express.Router();
-var usuario = require('')
+var usuario = require('../models/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/login', ( req , res , next)=>{
+//Esto solo realiza el proceso de autenticación(para el ejemplo),
+//pero no tiene elementos de SEGURIDAD
+router.post('/login', ( req , res , next )=>{
+  //console.log( req.body.email, req.body.passwd );
+  usuario.login(req.body.email,req.body.passwd,( e , d )=>{
+    if(d) {
+      res.send('Login correcto');
+      ses=req.session;
+      console.log(ses.id);
+
+      //crear la sesion
+      /*
+      1.-Reutilizar la sesion original del chrome
+      2.-hacer una nueva, desecgabdi la de web browser
+      */
+
+    } else {
+      res.json(e);
+    }
+
+  });
 
 });
+
 
 module.exports = router;
